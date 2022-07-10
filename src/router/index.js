179027170502638019da7e62e452196259da21d4
lencamo <207141481@qq.com>
2022-07-10 21:48:19 +0800

    导航前置守卫-访问控制权限、axios拦截器-身份过期处理

--- src/router/index.js
+++ src/router/index.js
@@ -30,14 +30,33 @@ const router = new VueRouter({
   routes
 })
 
+const whiteList = ['/login', '/reg']
+
 // 发生页面跳转时触发
 router.beforeEach((to, from, next) => {
-  // 初次：有token时，才触发获取用户信息的函数
-  // 处于登录状态时：跳转时不用重复触发
-  if ($store.state.token && !$store.state.userInfo.username) {
-    $store.dispatch('initUserInfo')
-  }
+  const token = $store.state.token
 
+  // 权限访问控制
+  if (token) {
+    // 如果有token, 证明已登录
+    if (!$store.state.userInfo.username) {
+      // 有token但是没有用户信息, 才去请求用户信息保存到vuex里
+      // 调用actions里方法请求数据
+      $store.dispatch('initUserInfo')
+      // 下次切换页面vuex里有用户信息数据就不会重复请求用户信息
+    }
+    next() // 路由放行
+  } else {
+    // 如果无token
+    // 【因为登录、注册页面也没有token，根据全局前置守卫的定义不做白名单处理，会一直处于跳转——触发——跳转循环中】
+    // 如果去的是白名单✨页面, 则放行
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      // 如果其他页面请强制拦截并跳转到登录页面
+      next('/login')
+    }
+  }
   next()
 })
 
--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import $store from '@/store/index'
+import $router from '@/router/index'
+// 引入element-ui弹窗
+import { Message } from 'element-ui'
 
 // 创建一个自定的axios方法(比原axios多了个基地址)
 // axios函数请求的url地址前面会被拼接基地址, 然后axios请求baseURL+url后台完整地址
@@ -8,7 +11,6 @@ const reqAxios = axios.create({
 })
 
 // 使用axios拦截器处理api接口调用时，部分参数重复调用问题
-
 reqAxios.interceptors.request.use(
   function (config) {
     // 在发送请求之前做些什么
@@ -25,5 +27,30 @@ reqAxios.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
+// 解决token过期问题
+reqAxios.interceptors.response.use(
+  function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response
+  },
+  function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    if (error.response.status === 401) {
+      // console.dir(error)
+      // 无效的 token
+      // 把 Vuex 中的 token 重置为空，并跳转到登录页面
+      $store.commit('updateToken', '')
+      $router.push('/login')
+
+      // 弹窗提示
+      Message.error('用户身份以过期！！')
+    }
+    return Promise.reject(error)
+  }
+)
+
 // 导出自定义的axios方法, 供外面调用传参发请求
 export default reqAxios
